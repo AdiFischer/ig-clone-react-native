6457465015cda2dd2145f4b7212ceb0f2bba949d
AdiFischer <adifischer1@gmail.com>
2022-11-10 10:51:45 -0500

    ig-clone-pictures,descriptions

---
+++ photolist.js
@@ -0,0 +1,47 @@
+const photolist = [
+    {
+        id: 1,
+        photoURL: 'https://images.unsplash.com/photo-1667853003724-fd11b2f8863b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTI3MA&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'mountains and a river',
+        userName: 'river',
+        profilePhoto: 'https://randomuser.me/api/portraits/women/1.jpg'
+    },
+    {
+        id: 2,
+        photoURL: 'https://images.unsplash.com/photo-1667369530650-ad459f616300?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTQ5NQ&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'guy holding a camera',
+        userName: 'guy',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/2.jpg'
+    },
+    {
+        id: 3,
+        photoURL: 'https://images.unsplash.com/photo-1666347943405-307bde77a9fb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU3MQ&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'ice',
+        userName: 'ice',
+        profilePhoto: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {
+        id: 4,
+        photoURL: 'https://images.unsplash.com/photo-1666943894918-5916e6d02ca7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTY2OA&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'woman looking at mountains',
+        userName: 'woman',
+        profilePhoto: 'https://randomuser.me/api/portraits/women/4.jpg'
+    },
+    {
+        id: 5,
+        photoURL: 'https://images.unsplash.com/photo-1665659145984-2d474f053e2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTc0Mw&ixlib=rb-1.2.1&q=80&w=1080',
+        description: 'camera on tile wall',
+        userName: 'camera',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/5.jpg'
+    },
+
+    {
+        id: 6,
+        photoURL: 'https://images.unsplash.com/photo-1665682597003-f31e65b59efd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MjEzNQ&ixlib=rb-1.2.1&q=80&w=1080',
+        description: '2 divers in water',
+        userName: 'water',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+]
+
+export default photolist;
